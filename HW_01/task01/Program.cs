47a9ea4b9a88699e7cf5bd7ee93862cd19798307
olgaKoshatis <ahethyfy@gmail.com>
2023-06-18 23:59:07 +0300

    Добавлены задачи 01, 02, 03, 04 с решением

---
+++ HW_01/task01/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using System;
+Console.WriteLine("Введите по очереди два числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b){
+    Console.WriteLine("Большее число " + a);
+}
+else{
+    Console.WriteLine("Большее число " + b);
+}
